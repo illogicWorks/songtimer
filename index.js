d4f057e56474d9d40337efe52fd09fbf524469e9
altrisi <altrisi.trillosierra@gmail.com>
2023-01-16 17:12:52 +0100

    Add timer code

--- index.js
+++ index.js
@@ -55,21 +55,59 @@ function toInt(id) {
     return parseInt(document.getElementById(id).value);
 }
 
+// record TimeInfo(Input hI, Input mI, Input sI, int h, int m, int s)
+let current = null;
+
 function handleClick() {
-    let time = toInt('h') * 60 * 60
-             + toInt('m') * 60
-             + toInt('s');
-    startTimer(time);
-}
+    let timeInfo = {};
+    timeInfo.hI = document.getElementById('h');
+    timeInfo.mI = document.getElementById('m');
+    timeInfo.sI = document.getElementById('s');
+    timeInfo.h = parseInt(timeInfo.hI.value);
+    timeInfo.m = parseInt(timeInfo.mI.value);
+    timeInfo.s = parseInt(timeInfo.sI.value);
 
-document.getElementById('start').onclick = handleClick;
-
-
-// time in seconds
-function startTimer(time) {
-    if (player == null || time == -1) alert('no video selected')
+    let time = h * 60 * 60 + m * 60 + s;
     console.log('Starting ' + time + ' second timer');
+    if (player == null || time == -1) {
+        alert('no video selected or not yet loaded');
+        return;
+    }
     setTimeout(function() {
         player.playVideo();
     }, time * 1000 - duration * 1000);
+    hI.disabled = true;
+    mI.disabled = true;
+    sI.disabled = true;
+    this.disabled = true;
+    current = timeInfo;
+    setInterval(tickTimer, 1000);
 }
+
+function tickTimer() {
+    let {hI, mI, sI, h, m, s} = current;
+    s--;
+    if (!h && !m && !s) {
+        clearInterval(this);
+        hI.disabled = true;
+        mI.disabled = true;
+        sI.disabled = true;
+        document.getElementById('start').disabled = false;
+        current = null;
+    }
+    if (s < 0) {
+        s = 59;
+        m--;
+        if (m < 0) {
+            m = 59;
+            current.h--;
+            hI.value = current.h;
+        }
+        current.m = m;
+        mI.value = m;
+    }
+    current.s = s;
+    sI.value = s;
+}
+
+document.getElementById('start').onclick = handleClick;
