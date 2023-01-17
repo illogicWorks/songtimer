2e8cc4e89b93e0d2c68482d8b2e2143a2e076a10
altrisi <altrisi.trillosierra@gmail.com>
2023-01-17 13:37:21 +0100

    Revert 'Simplify timeout call'
    This commit reverts 0db743571a729b2d379e22865a31aebe5f906613

--- index.js
+++ index.js
@@ -91,7 +91,9 @@ function doStart() {
         alert('no video selected or not yet loaded');
         return;
     }
-    let timeout = setTimeout(player.playVideo, time * 1000 - duration * 1000);
+    let timeout = setTimeout(function() {
+        player.playVideo();
+    }, time * 1000 - duration * 1000);
     hI.disabled = true;
     mI.disabled = true;
     sI.disabled = true;
